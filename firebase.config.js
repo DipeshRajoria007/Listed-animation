5425a9bf6c7aa8acd21de5f3308a6a52b18e99a5
Dipesh Rajoria <dipeshrajoria45@gmail.com>
2023-06-23 17:36:22 +0530

    updated firebase config

--- firebase.config.js
+++ firebase.config.js
@@ -2,14 +2,13 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyBQYBXT3AZ3X1Ama3JyE1kAAfd9Wu0F8BQ",
-  authDomain: "restaurantapp-68792.firebaseapp.com",
-  databaseURL: "https://restaurantapp-68792-default-rtdb.firebaseio.com",
-  projectId: "restaurantapp-68792",
-  storageBucket: "restaurantapp-68792.appspot.com",
-  messagingSenderId: "299927380899",
-  appId: "1:299927380899:web:73cd5acef9ae045eb7f016",
-  measurementId: "G-1PYZD0F57R",
+  apiKey: "AIzaSyAbfshVYk5P3FYKYYNZMKz3B8jq3seRhCs",
+  authDomain: "listed-animation.firebaseapp.com",
+  projectId: "listed-animation",
+  storageBucket: "listed-animation.appspot.com",
+  messagingSenderId: "321176902132",
+  appId: "1:321176902132:web:d081b82cdf17f81067c1a1",
+  measurementId: "G-LZF025R0CF",
 };
 const app = getApps.lenght > 0 ? getApp() : initializeApp(firebaseConfig);
 
